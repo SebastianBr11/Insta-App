59552bd7f84f5b87ba9e1e6b261b79a0678b8a45
Sebastian Breit <flockean11@gmail.com>
2020-08-31 15:40:20 +0200

    f

--- index.js
+++ index.js
@@ -10,6 +10,62 @@ const PORT = process.env.PORT || 8000;
 
 app.use(express.json());
 
+// Test for bypassing heroku 30 sec timeout
+
+const extendTimeoutMiddleware = (req, res, next) => {
+  const space = " ";
+  let isFinished = false;
+  let isDataSent = false;
+
+  // Only extend the timeout for API requests
+  if (!req.url.includes("/api")) {
+    next();
+    return;
+  }
+
+  res.once("finish", () => {
+    isFinished = true;
+  });
+
+  res.once("end", () => {
+    isFinished = true;
+  });
+
+  res.once("close", () => {
+    isFinished = true;
+  });
+
+  res.on("data", data => {
+    // Look for something other than our blank space to indicate that real
+    // data is now being sent back to the client.
+    if (data !== space) {
+      isDataSent = true;
+    }
+  });
+
+  const waitAndSend = () => {
+    setTimeout(() => {
+      // If the response hasn't finished and hasn't sent any data back....
+      if (!isFinished && !isDataSent) {
+        // Need to write the status code/headers if they haven't been sent yet.
+        if (!res.headersSent) {
+          res.writeHead(202);
+        }
+
+        res.write(space);
+
+        // Wait another 15 seconds
+        waitAndSend();
+      }
+    }, 15000);
+  };
+
+  waitAndSend();
+  next();
+};
+
+app.use(extendTimeoutMiddleware);
+
 app.get("/api/search", async (req, res) => {
   const { q } = req.query;
   const results = await pupReq(q).catch(e => console.log(e));
